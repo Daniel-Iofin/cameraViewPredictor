19f724c1840ff0cbbb2f1b76cd480cca4e9312b2
Daniel-Iofin <daniel.iofin26@trinityschoolnyc.org>
2024-09-16 22:39:10 -0400

    Sorts vertex angle data

--- sceneSimulation/sceneSimulation.js
+++ sceneSimulation/sceneSimulation.js
@@ -1,26 +1,33 @@
-function getVertexAngles(vertices) {
+function getVertexAngles(vertices, camera) {
     const vertexAngles = [];
-    this.vertices.forEach(vertex => {
+    vertices.forEach(vertex => {
         let vertexId = vertex.id;
         let deltaX = vertex.x - camera.x;
-        let deltaY = vertexY.x - camera.y;
+        let deltaY = vertexY.y - camera.y;
 
         let angleFromHorizontal = Math.atan(deltaX/deltaY); // Calculate complementary angle --> angle for camera facing forward
         let angleFromCameraCenter = angleFromHorizontal - camera.angle;
-        vertexAngles.push({vertexId, angleFromCameraCenter})
+
+        vertexAngles.push([vertexId, angleFromCameraCenter])
     });
     return vertexAngles;
 }
 
+function getViewLines(cameraSweep, vertices, vertexAngles, lines) {
+    let vertexAnglesSorted = vertexAngles.sort((a, b) => a[1] - b[1]);
+    console.log(vertexAnglesSorted)
+    // get active lines at each point --> which one is closest
+    return -1;
+}
+
 class simulate2D {
     constructor(vertices, lines) {
-        this.vertices = vertices;
-        this.lines = lines;
+        this.vertices = vertices; // {id, x, y}
+        this.lines = lines; // {id, line1Id, line2Id}
     }
 
-    predict(camera) {
-        const vertexAngles = getVertexAngles(vertices);
+    predict(camera) { // {x, y, angle, sweep}  angle is rotation of camera, sweep is how wide camera sees
+        const vertexAngles = getVertexAngles(this.vertices);
+        const viewPartition = getViewLines(camera.sweep, this.vertices, vertexAngles, this.lines);  // Which line is seen for which angle
     }
-
-
 }
