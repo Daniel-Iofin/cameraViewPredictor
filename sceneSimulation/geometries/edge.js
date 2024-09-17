1ec4f917cfe48ac0acf19dc43ec7a318e8571725
Daniel-Iofin <daniel.iofin26@trinityschoolnyc.org>
2024-09-17 16:52:28 -0400

    Sorts the edges by which edge starts further to the left within camera view

--- sceneSimulation/geometries/edge.js
+++ sceneSimulation/geometries/edge.js
@@ -4,4 +4,11 @@ export default class Edge {
         this.vertex1Id = vertex1Id;
         this.vertex2Id = vertex2Id;
     }
+
+    getLeftVertexAngle(vertices) {
+        let vertex1 = vertices.find(vertex => vertex.id == this.vertex1Id);
+        let vertex2 = vertices.find(vertex => vertex.id == this.vertex2Id);
+
+        return Math.min(vertex1.cameraAngle, vertex2.cameraAngle);
+    }
 }
--- sceneSimulation/sceneSimulation.js
+++ sceneSimulation/sceneSimulation.js
@@ -16,28 +16,11 @@ function getVertexAngles(vertices, camera) {
 }
 
 function orderedges(vertices, edges) {
-    let orderededges = [];
-
-    edges.forEach(edge => {
-        let vertex1Id = edge.vertex1Id;
-        let vertex1 = vertices.find(vertex => vertex.id == vertex1Id);
-        let vertex2Id = edge.vertex2Id;
-        let vertex2 = vertices.find(vertex => vertex.id == vertex2Id)
-
-        if (vertex1.cameraAngle<vertex2.cameraAngle) { // bug from here down
-            console.log("here")
-            orderededges.push(edge);
-        } else {
-            edge.vertex1Id = vertex2Id;
-            edge.vertex2Id = vertex1Id;
-            orderededges.push(edge);
-        }
-    })
-    
-    return orderededges;
+    edges.sort((a, b) => a.getLeftVertexAngle(vertices) - b.getLeftVertexAngle(vertices));
+    return edges;
 }
 
-function getViewedges(vertices, edges) {
+function getViewEdges(vertices, edges) {
     vertices = vertices.sort((a, b) => a.cameraAngle - b.cameraAngle);
     const orderededges = orderedges(vertices, edges);
     console.log(orderededges);
@@ -46,17 +29,16 @@ function getViewedges(vertices, edges) {
 
 class Simulate2D {
     constructor(vertices, edges, camera) {
-        this.vertices = vertices; // {id, x, y}
-        this.edges = edges; // {id: {vertex1Id:_, vertex2Id:_}}
+        this.vertices = vertices;
+        this.edges = edges;
         this.camera = camera;
     }
 
-    predict() { // {x, y, angle, sweep}  angle is rotation of camera, sweep is how wide camera sees
-        this.vertices = getVertexAngles(this.vertices, this.camera);
-        const viewPartition = getViewedges(this.vertices, this.edges);  // Which edge is seen for which angle
+    predict() {
+        this.vertices = getVertexAngles(this.vertices, this.camera); // angle each vertex is at from centerline of camera view
+        const viewPartition = getViewEdges(this.vertices, this.edges);  // Which edge is seen for which angle
     }
 }
-//getViewedges(0, 0, [[5, 4], [7, 10], [9, 5]], [{"vertex1Id": 7, "vertex2Id": 9},{"vertex1Id": 5, "vertex2Id": 9}])
 
 let vertices = [new Vertex(0, 1, 4), new Vertex(1, 2, 5), new Vertex(2, 4, 4), new Vertex(3, 5, 4)];
 let edges = [new Edge(0, 0, 2), new Edge(1, 1, 3)];
