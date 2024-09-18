480e549b122571497e4ea515f039a945f1e23f1f
Daniel-Iofin <daniel.iofin26@trinityschoolnyc.org>
2024-09-18 19:10:13 -0400

    intersection() finds point of intersection of 2 lines

--- sceneSimulation/geometries/equation.js
+++ sceneSimulation/geometries/equation.js
@@ -1,4 +1,5 @@
 import Point from '/vertex.js';
+
 export default class Equation {
     constructor(m, b) {
         this.m = m;
@@ -10,11 +11,18 @@ export default class Equation {
             return new Equation(this.m, this.b);
         } else if (equation.m==this.m) {
             return NaN;
+        } else if (equation.m==Infinity) { // when m=Infinity, b is the x-coordinate
+            let intersectionX = equation.b;
+            let intersectionY = this.m*intersectionX+this.b;
+            return Point(intersectionX, intersectionY);
+        } else if (this.m==Infinity) { // when m=Infinity, b is the x-coordinate
+            let intersectionX = this.b;
+            let intersectionY = equation.m*intersectionX+equation.b;
+            return Point(intersectionX, intersectionY);
         }
 
         let intersectionX = (equation.b-this.b)/(this.m-equation.m);
         let intersectionY = this.m*intersectionX+this.b;
-
         return Point(intersectionX, intersectionY);
     }
 }
